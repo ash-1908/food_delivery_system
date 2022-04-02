00c8655b2fd89c2b114851db0bdf9f2a9797ca23
bhoomijaray <bhoomijaray@gmail.com>
2022-04-02 23:39:22 +0530

    Bhoomija-Login class created.

---
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/model/Login.java
@@ -0,0 +1,41 @@
+package com.cg.FDS.model;
+
+public class Login {
+	
+	private String userId;
+	private String userName;
+	private String password;
+	public Login() {
+		super();
+	}
+	public Login(String userId, String userName, String password) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Login [userId=" + userId + ", userName=" + userName + ", password=" + password + "]";
+	}
+	
+
+}
