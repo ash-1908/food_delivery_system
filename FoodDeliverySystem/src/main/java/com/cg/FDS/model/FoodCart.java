1a8288efa885acba5105f4e24ab5d09677c400e3
Ashi <ashisrivastava880@gmail.com>
2022-04-03 00:55:38 +0530

    Ashi- FoodCart class created

--- FoodDeliverySystem/src/main/java/com/cg/FDS/model/FoodCart.java
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/model/FoodCart.java
@@ -1,41 +1,38 @@
 package com.cg.FDS.model;
 
-import java.util.List;
-
 public class FoodCart {
-private String cartId;
-private Customer customer;
-private List<Item> itemList;
-public FoodCart() {
-	super();
-}
-public FoodCart(String cartId, Customer customer, List<Item> itemList) {
-	super();
-	this.cartId = cartId;
-	this.customer = customer;
-	this.itemList = itemList;
-}
-public String getCartId() {
-	return cartId;
-}
-public void setCartId(String cartId) {
-	this.cartId = cartId;
-}
-public Customer getCustomer() {
-	return customer;
-}
-public void setCustomer(Customer customer) {
-	this.customer = customer;
-}
-public List<Item> getItemList() {
-	return itemList;
-}
-public void setItemList(List<Item> itemList) {
-	this.itemList = itemList;
-}
-@Override
-public String toString() {
-	return "FoodCart [cartId=" + cartId + ", customer=" + customer + ", itemList=" + itemList + "]";
-}
-
+	private String cartId;
+	private Customer customer;
+	private List<Item> itemList;
+	public FoodCart() {
+		super();
+	}
+	public FoodCart(String cartId, Customer customer, List<Item> itemList) {
+		super();
+		this.cartId = cartId;
+		this.customer = customer;
+		this.itemList = itemList;
+	}
+	public String getCartId() {
+		return cartId;
+	}
+	public void setCartId(String cartId) {
+		this.cartId = cartId;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public List<Item> getItemList() {
+		return itemList;
+	}
+	public void setItemList(List<Item> itemList) {
+		this.itemList = itemList;
+	}
+	@Override
+	public String toString() {
+		return "FoodCart [cartId=" + cartId + ", customer=" + customer + ", itemList=" + itemList + "]";
+	}
 }
