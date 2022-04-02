803f4682cdcea7d6a96a1e7a816735de0eb4e193
bhoomijaray <bhoomijaray@gmail.com>
2022-04-02 23:48:16 +0530

    Bhoomija-Resturant class created.

---
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/model/Resturant.java
@@ -0,0 +1,68 @@
+package com.cg.FDS.model;
+
+import java.util.List;
+
+public class Resturant {
+	
+	private String resturantId;
+	private String resturantName;
+	private Address address;
+	private List<Item> itemList;
+	private String managerName;
+	private String contactNumber;
+	public Resturant() {
+		super();
+	}
+	public Resturant(String resturantId, String resturantName, Address address, List<Item> itemList, String managerName,
+			String contactNumber) {
+		super();
+		this.resturantId = resturantId;
+		this.resturantName = resturantName;
+		this.address = address;
+		this.itemList = itemList;
+		this.managerName = managerName;
+		this.contactNumber = contactNumber;
+	}
+	public String getResturantId() {
+		return resturantId;
+	}
+	public void setResturantId(String resturantId) {
+		this.resturantId = resturantId;
+	}
+	public String getResturantName() {
+		return resturantName;
+	}
+	public void setResturantName(String resturantName) {
+		this.resturantName = resturantName;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public List<Item> getItemList() {
+		return itemList;
+	}
+	public void setItemList(List<Item> itemList) {
+		this.itemList = itemList;
+	}
+	public String getManagerName() {
+		return managerName;
+	}
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	@Override
+	public String toString() {
+		return "Resturant [resturantId=" + resturantId + ", resturantName=" + resturantName + ", address=" + address
+				+ ", managerName=" + managerName + ", contactNumber=" + contactNumber + "]";
+	}
+	
+}
