01d99d09cfb2c3cdf654415d18d971fe77212af0
Ashi <ashisrivastava880@gmail.com>
2022-04-07 14:52:46 +0530

    item dao & Service created

---
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/service/IItemServiceImpl.java
@@ -0,0 +1,71 @@
+package com.cg.FDS.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cg.FDS.dao.IItemRepository;
+import com.cg.FDS.model.Category;
+import com.cg.FDS.model.Item;
+import com.cg.FDS.model.Restaurant;
+
+public class IItemServiceImpl implements  IItemService{
+	@Autowired
+	IItemRepository itemRepo;
+
+	public Item addItem(Item item) {
+		// TODO Auto-generated method stub
+		itemRepo.save(item);
+		return item;
+	}
+
+	public Item updateItem(Item item) {
+		// TODO Auto-generated method stub
+		if(itemRepo.existsById(item.getItemId())) {
+			itemRepo.save(item);
+			return item;
+			
+		}
+		return null;
+	}
+
+	public Item viewItem(Item item) {
+		// TODO Auto-generated method stub
+		System.out.println(item);
+		return item;
+	}
+
+	public Item removeItem(Item item) {
+		// TODO Auto-generated method stub
+		if(itemRepo.existsById(item.getItemId())) {
+			itemRepo.deleteById(item.getItemId());
+			return item;
+		}
+		return null;
+	}
+
+	public List<Item> viewAllItems(Category cat) {
+		// TODO Auto-generated method stub
+		List<Item> itemList=itemRepo.viewAllItems(cat);
+		for(Item it:itemList)
+			System.out.println(it);
+		return itemList;
+	}
+
+	public List<Item> viewAllItems(Restaurant res) {
+		// TODO Auto-generated method stub
+		List<Item> itemList1=itemRepo.viewAllItems(res);
+		for(Item it1:itemList1)
+			System.out.println(it1);
+		return itemList1;
+	}
+
+	public List<Item> viewAllItemsByName(String name) {
+		// TODO Auto-generated method stub
+		List<Item> nameList=itemRepo.viewAllItemsByName(name);
+		for(Item i : nameList)
+			System.out.println(i);
+		return nameList;
+	}
+
+}
