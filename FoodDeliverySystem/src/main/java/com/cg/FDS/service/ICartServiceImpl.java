a464469cace0fab6bda365e539e13777b8ba9cb7
Ashi <ashisrivastava880@gmail.com>
2022-04-08 10:46:33 +0530

    dao & service

--- FoodDeliverySystem/src/main/java/com/cg/FDS/service/ICartServiceImpl.java
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/service/ICartServiceImpl.java
@@ -9,28 +9,29 @@ import com.cg.FDS.model.Item;
 public class ICartServiceImpl implements ICartService{
 	@Autowired
 	ICartRepository cartRepo;
-
+	@Override
 	public FoodCart addItemToCart(FoodCart cart, Item item) {
 		// TODO Auto-generated method stub
-		cartRepo.addI
+
+	
 		return null;
 			}
-
+	@Override
 	public FoodCart increaseQuantity(FoodCart cart, Item item, int quantity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Override
 	public FoodCart reduceQuantity(FoodCart cart, Item item, int quantity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Override
 	public FoodCart removeItem(FoodCart cart, Item item) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Override
 	public FoodCart clearCart(FoodCart cart) {
 		// TODO Auto-generated method stub
 		return null;
