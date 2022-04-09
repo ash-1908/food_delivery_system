c762c91f02b813e9a324a66f68b41b811220a965
Ashi <ashisrivastava880@gmail.com>
2022-04-09 13:34:31 +0530

    +service implementation

--- FoodDeliverySystem/src/main/java/com/cg/FDS/service/ICartServiceImpl.java
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/service/ICartServiceImpl.java
@@ -12,29 +12,45 @@ public class ICartServiceImpl implements ICartService{
 	@Override
 	public FoodCart addItemToCart(FoodCart cart, Item item) {
 		// TODO Auto-generated method stub
-
-	
-		return null;
-			}
+		cart.setItemList(cart.getItemList().add(item));
+		return cart;
+    }
 	@Override
 	public FoodCart increaseQuantity(FoodCart cart, Item item, int quantity) {
 		// TODO Auto-generated method stub
-		return null;
+        for(Item i: cart.getItemList()) {
+            if(i.getItemId() == item.getItemId()) {
+                i.setQuantity(i.getQuantity() + quantity);
+            }
+        }
+		return cart;
 	}
 	@Override
 	public FoodCart reduceQuantity(FoodCart cart, Item item, int quantity) {
 		// TODO Auto-generated method stub
-		return null;
+		for(Item i: cart.getItemList()) {
+            if(i.getItemId() == item.getItemId()) {
+                int existingQuantity = i.getQuantity();
+                if(existingQuantity - quantity <= 0)
+                 i.setQuantity(0);
+                else
+                 i.setQuantity(existingQuantity - quantity);
+            }
+        }
+		return cart;
 	}
 	@Override
 	public FoodCart removeItem(FoodCart cart, Item item) {
 		// TODO Auto-generated method stub
-		return null;
+        ArrayList<Item> iList = cart.getItemList();
+        iList = iList.stream().filter((i)-> i.getItemId != item.getItemId());
+		return cart;
 	}
 	@Override
 	public FoodCart clearCart(FoodCart cart) {
 		// TODO Auto-generated method stub
-		return null;
+		cart.setItemList(new ArrayList<Item>());
+		return cart;
 	}
 
 }
