012516b5d6f24804d57455ba7190a0e00a92891f
Anmol Sharma <anmolsh.1908@gmail.com>
2022-04-10 15:20:40 +0530

    debugging: updated address and restaurant service implementation

--- FoodDeliverySystem/src/main/java/com/cg/FDS/service/IAddressServiceImpl.java
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/service/IAddressServiceImpl.java
@@ -1,10 +1,12 @@
 package com.cg.FDS.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cg.FDS.dao.IAddressRepository;
 import com.cg.FDS.model.Address;
 
+@Service
 public class IAddressServiceImpl {
 
 	@Autowired
--- FoodDeliverySystem/src/main/java/com/cg/FDS/service/IRestaurantServiceImpl.java
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/service/IRestaurantServiceImpl.java
@@ -1,11 +1,14 @@
 package com.cg.FDS.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cg.FDS.dao.IItemRepository;
 import com.cg.FDS.dao.IRestaurantRepository;
+import com.cg.FDS.model.Address;
 import com.cg.FDS.model.Item;
 import com.cg.FDS.model.Restaurant;
 
@@ -21,14 +24,25 @@ public class IRestaurantServiceImpl implements IRestaurantService {
 	@Autowired
 	IItemServiceImpl itemServ;
 
+	@Autowired
+	IItemRepository itemRepo;
+
 	@Override
 	public Restaurant addRestaurant(Restaurant rest) {
 		// TODO Auto-generated method stub
-		addrServ.addAddress(rest.getAddress());
+		Address adr = addrServ.getAddress(rest.getAddress().getAddressId());
+		List<Item> itemList = new ArrayList<>();
 		for (Item i : rest.getItemList()) {
-			itemServ.addItem(i);
+			itemList.add(itemRepo.findById(i.getItemId()).get());
 		}
+		rest.setAddress(null);
+		rest.setItemList(null);
 		resRepo.save(rest);
+
+		rest.setAddress(adr);
+		rest.setItemList(itemList);
+		resRepo.save(rest);
+
 		return rest;
 	}
 
