4d994e7e65753e2a3ebd3b68389d5d56719b5ecd
Ankita <ankita.sahu2010@gmail.com>
2022-04-10 10:46:16 +0530

    done

--- FoodDeliverySystem/src/main/java/com/cg/FDS/exception/CategoryException.java
+++
@@ -1,9 +0,0 @@
-package com.cg.FDS.exception;
-
-public class CategoryException extends Exception{
-	
-	public CategoryException(String str) {
-		super(str);
-	}
-
-}
--- FoodDeliverySystem/src/main/java/com/cg/FDS/exception/OrderDetailsException.java
+++
@@ -1,9 +0,0 @@
-package com.cg.FDS.exception;
-
-public class OrderDetailsException extends Exception {
-	
-	public OrderDetailsException(String str) {
-		super(str);
-	}
-
-}
