36469f103e91a263534f993f44002e14a2e2dbed
Anmol Sharma <anmolsh.1908@gmail.com>
2022-04-10 14:00:57 +0530

    debugging: added a single global exception handler which previously was divided for separate classes

---
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/exception/global/GlobalExceptionHandler.java
@@ -0,0 +1,271 @@
+package com.cg.FDS.exception.global;
+
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.cg.FDS.exception.bill.BillAlreadyExists;
+import com.cg.FDS.exception.bill.BillNotFoundException;
+import com.cg.FDS.exception.bill.InvalidBillCustomerIdException;
+import com.cg.FDS.exception.bill.InvalidBillDateException;
+import com.cg.FDS.exception.cart.AlreadyExistInCartException;
+import com.cg.FDS.exception.category.CategoryAlreadyExists;
+import com.cg.FDS.exception.category.CategoryNotFoundException;
+import com.cg.FDS.exception.category.NullCategoryException;
+import com.cg.FDS.exception.customer.CustomerAlreadyExistsException;
+import com.cg.FDS.exception.customer.CustomerNotFoundException;
+import com.cg.FDS.exception.customer.NullCustomerException;
+import com.cg.FDS.exception.item.ItemCannotRemoved;
+import com.cg.FDS.exception.item.ItemIdNotExistException;
+import com.cg.FDS.exception.item.ItemNotFoundException;
+import com.cg.FDS.exception.item.ItemNotFoundInCategoryException;
+import com.cg.FDS.exception.item.NoItemWithThisNameException;
+import com.cg.FDS.exception.item.QuantityIsNullException;
+import com.cg.FDS.exception.login.LoginAlreadyExistsException;
+import com.cg.FDS.exception.login.LoginNotFoundException;
+import com.cg.FDS.exception.order.NullOrderException;
+import com.cg.FDS.exception.order.OrderAlreadyExistsException;
+import com.cg.FDS.exception.order.OrderNotFoundException;
+import com.cg.FDS.exception.restaurant.NullRestaurantException;
+import com.cg.FDS.exception.restaurant.RestaurantAlreadyExists;
+import com.cg.FDS.exception.restaurant.RestaurantNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	// Bill exceptions
+
+	@ExceptionHandler(BillAlreadyExists.class)
+	public @ResponseBody ErrorInfo addBillException(BillAlreadyExists e, HttpServletRequest req) {
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(BillNotFoundException.class)
+	public @ResponseBody ErrorInfo updateBillException(BillNotFoundException e, HttpServletRequest req) {
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(BillNotFoundException.class)
+	public @ResponseBody ErrorInfo removeBillException(BillNotFoundException e, HttpServletRequest req) {
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(BillNotFoundException.class)
+	public @ResponseBody ErrorInfo viewBillException(BillNotFoundException e, HttpServletRequest req) {
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(InvalidBillDateException.class)
+	public @ResponseBody ErrorInfo viewBillbyDateException(BillNotFoundException e, HttpServletRequest req) {
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(InvalidBillCustomerIdException.class)
+	public @ResponseBody ErrorInfo viewBillbyCustomerIdException(BillNotFoundException e, HttpServletRequest req) {
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	// Category exceptions
+	@ExceptionHandler(CategoryAlreadyExists.class)
+	public @ResponseBody ErrorInfo addCategoryException(CategoryAlreadyExists e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(CategoryNotFoundException.class)
+	public @ResponseBody ErrorInfo updateCategoryException(CategoryNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(CategoryNotFoundException.class)
+	public @ResponseBody ErrorInfo removeCategoryException(CategoryNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(NullCategoryException.class)
+	public @ResponseBody ErrorInfo viewCategoryException(NullCategoryException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	// Customer exceptions
+	@ExceptionHandler(CustomerAlreadyExistsException.class)
+	public @ResponseBody ErrorInfo addException(CustomerAlreadyExistsException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(CustomerNotFoundException.class)
+	public @ResponseBody ErrorInfo updateException(CustomerNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(CustomerNotFoundException.class)
+	public @ResponseBody ErrorInfo removeException(CustomerNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(NullCustomerException.class)
+	public @ResponseBody ErrorInfo viewException(NullCustomerException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(CustomerNotFoundException.class)
+	public @ResponseBody ErrorInfo viewAllException(CustomerNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	// FoodCart exceptions
+	@ExceptionHandler(ItemIdNotExistException.class)
+	public @ResponseBody ErrorInfo increaseQuantityException(ItemIdNotExistException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(QuantityIsNullException.class)
+	public @ResponseBody ErrorInfo reduceQuantityException(QuantityIsNullException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(ItemCannotRemoved.class)
+	public @ResponseBody ErrorInfo removeItemException(AlreadyExistInCartException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	// Item exceptions
+	@ExceptionHandler(ItemNotFoundException.class)
+	public @ResponseBody ErrorInfo updateException(ItemNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(ItemNotFoundException.class)
+	public @ResponseBody ErrorInfo removeItemException(ItemNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(ItemNotFoundInCategoryException.class)
+	public @ResponseBody ErrorInfo viewAllItemsException(ItemNotFoundInCategoryException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(NoItemWithThisNameException.class)
+	public @ResponseBody ErrorInfo viewAllItemsByNameException(NoItemWithThisNameException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	// Login exceptions
+	@ExceptionHandler(LoginAlreadyExistsException.class)
+	public @ResponseBody ErrorInfo addException(LoginAlreadyExistsException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(LoginNotFoundException.class)
+	public @ResponseBody ErrorInfo signOut(LoginNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	// Order exceptions
+	@ExceptionHandler(OrderAlreadyExistsException.class)
+	public @ResponseBody ErrorInfo addException(OrderAlreadyExistsException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(OrderNotFoundException.class)
+	public @ResponseBody ErrorInfo updateException(OrderNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(OrderNotFoundException.class)
+	public @ResponseBody ErrorInfo removeException(OrderNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(OrderNotFoundException.class)
+	public @ResponseBody ErrorInfo viewException(OrderNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(NullOrderException.class)
+	public @ResponseBody ErrorInfo viewAllByRestException(NullOrderException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(NullOrderException.class)
+	public @ResponseBody ErrorInfo viewAllByCustException(NullOrderException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	// Restaurant exceptions
+	@ExceptionHandler(RestaurantAlreadyExists.class)
+	public @ResponseBody ErrorInfo addRestaurantException(RestaurantAlreadyExists e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(RestaurantNotFoundException.class)
+	public @ResponseBody ErrorInfo updateRestaurantException(RestaurantNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+
+	}
+
+	@ExceptionHandler(RestaurantNotFoundException.class)
+	public @ResponseBody ErrorInfo removeRestaurantException(RestaurantNotFoundException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(NullRestaurantException.class)
+	public @ResponseBody ErrorInfo viewRestaurantException(NullRestaurantException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(NullRestaurantException.class)
+	public @ResponseBody ErrorInfo viewNearByRestaurantException(NullRestaurantException e, HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+
+	@ExceptionHandler(NullRestaurantException.class)
+	public @ResponseBody ErrorInfo viewRestaurantByItemNameException(NullRestaurantException e,
+			HttpServletRequest req) {
+
+		return new ErrorInfo(LocalDateTime.now(), e.getMessage(), req.getRequestURI());
+	}
+}
