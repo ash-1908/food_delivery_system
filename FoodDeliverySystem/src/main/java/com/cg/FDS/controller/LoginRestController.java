af9e0d549d5ecf41b7a2b506a60542f82013b9c5
Ankita <ankita.sahu2010@gmail.com>
2022-04-11 00:14:38 +0530

    edit

--- FoodDeliverySystem/src/main/java/com/cg/FDS/controller/LoginRestController.java
+++ FoodDeliverySystem/src/main/java/com/cg/FDS/controller/LoginRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.FDS.dao.ILoginRepository;
@@ -15,6 +16,7 @@ import com.cg.FDS.model.Login;
 import com.cg.FDS.service.ILoginServiceImpl;
 
 @RestController
+@RequestMapping("/rest")
 @CrossOrigin("*")
 public class LoginRestController {
 	
